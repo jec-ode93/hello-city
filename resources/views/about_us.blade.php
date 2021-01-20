7caf5a153e2dd9251962fa6ac8d398c7f24b37a9
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-01-19 22:15:14 -0500

    Add about page  and update links

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+       <title>About Us | Hello City</title>
+
+       
+    </head>
+    
+    <body>
+        <!-- <h1>Hello from Quebec!</h1> -->
+        <p>Built with &hearts; by JEC-ODE93</p>
+        <p><a href="/">Revenir vers la page d'accueil</a> </p>
+
+
+    </body>
+    <footer>
+        <p>&copy; copyright {{date('Y')}} &middot;</p>
+    </footer>
+</html>
